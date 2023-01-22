d2a45a7a78ae2e8b7b5884dce6c85a632deb297b
utoktoomuch <105897681+utoktoomuch@users.noreply.github.com>
2023-01-22 06:04:48 -0500

    Changes
    Changing the function of QR code

--- src/UserInterface.py
+++ src/UserInterface.py
@@ -168,7 +168,7 @@ class Ui_Widget(object):
 
         self.btn_generatePassword.setText(
             f'Password entropy: {int(entropy)} bits \nThis Password is {password_strength} \nClick to generate another')
-
+    '''
     def genQRCode(self, Widget):
         file_path = 'qr.png'
         file_reader = JSONFileReader('vault.json')
@@ -181,7 +181,7 @@ class Ui_Widget(object):
 
         cv2.imshow("QR", img)
         cv2.waitKey(0)
-
+    '''
     def retranslateUi(self, Widget):
         _translate = QtCore.QCoreApplication.translate
         Widget.setWindowTitle(_translate("Widget", "Widget"))
